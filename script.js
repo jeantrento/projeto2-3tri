d0477dd4edf173803b312bd7d95b0aa7aa953b43
jeantrento <168451755+jeantrento@users.noreply.github.com>
2024-10-30 15:32:22 -0300

    .

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.getElementById('gradeForm').addEventListener('submit', function(event) 
         return;
     }
 
-    if (grade >= 6) {
+    if (grade >= 60) {
         result.textContent = 'Aprovado!';
         result.style.color = '#ff9800'; /* laranja */
     } else {
